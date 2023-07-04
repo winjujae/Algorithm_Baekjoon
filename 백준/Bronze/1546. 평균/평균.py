b225335460e98128f3b1620cf5f65e0b9066a539
ZB053 <136467253+winjujae@users.noreply.github.com>
2023-07-05 02:45:39 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,5 +1,5 @@
-N = int(input())  # 시험 본 과목의 개수 N을 입력받습니다.
-scores = list(map(int, input().split()))  # 세준이의 현재 성적을 입력받아 리스트 scores에 저장합니다.
+N = int(input())
+scores = list(map(int, input().split()))
 total = sum(scores)
 max_score = max(scores)
 new_average = total / N * 100 / max_score

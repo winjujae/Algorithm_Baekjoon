de13178e3507e742a4b59dc1056e2e5542f8e7a8
ZB053 <136467253+winjujae@users.noreply.github.com>
2023-12-11 03:01:25 +0900

    Update 코딩은 체육과목 입니다.py

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
@@ -3,3 +3,11 @@ if a%4 != 0 :
   print("long " * ((a//4)+1) + "int")
 else :
   print("long " * (a//4) + "int")
+
+#2
+n = int(input())
+k = n // 4
+str = ''
+for _ in range(1,k+1):
+    str += "long "
+print(str + 'int')

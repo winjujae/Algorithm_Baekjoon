b9ca7ba575160269da7ec32e4cbdc06ea756d125
ZB053 <136467253+winjujae@users.noreply.github.com>
2023-12-11 01:41:45 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
@@ -1,6 +1,5 @@
-n = int(input())
-k = n // 4
-str = ''
-for i in range(1,k+1):
-    str += "long "
-print(str + 'int')
+a = int(input())
+if a%4 != 0 :
+  print("long " * ((a//4)+1) + "int")
+else :
+  print("long " * (a//4) + "int")

46684c5c80aae6d2143f3bd5fd2f98d8c4b5ff8c
ZB053 <136467253+winjujae@users.noreply.github.com>
2023-12-14 11:11:20 +0900

    Update 숫자의 개수.py

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,6 +1,18 @@
 a = int(input())
 b = int(input())
 c = int(input())
+str1 = str(a*b*c)
+for i in range(10) :
+  cnt = 0
+  for j in range(len(str1)) :
+    if str(i) == str1[j] :
+      cnt += 1
+  print(cnt)
+
+#2
+a = int(input())
+b = int(input())
+c = int(input())
 str1 = str(a * b * c)
 
 for i in range(10):

6af98a063f3bb2d70d6b7316764dcdd42e925aa7
ZB053 <136467253+winjujae@users.noreply.github.com>
2023-12-17 23:59:14 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,6 +1,12 @@
-palindrome = input()
-
-if palindrome[::1] == palindrome[::-1]:
-    print(1)
-else:
-    print(0)
+n = input()
+lst1 = []
+cnt = 0
+for i in n :
+  lst1.append(i)
+for i in range(len(n)) :
+  if lst1[i] == lst1[len(n)-1-i] :
+    cnt += 1
+if cnt == len(n) :
+  print("1")
+else :
+  print("0")

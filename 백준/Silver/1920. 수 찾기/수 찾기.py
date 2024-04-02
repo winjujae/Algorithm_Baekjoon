a28180ba0658f3ae50f7385a5344876a146a85c1
ZB053 <136467253+winjujae@users.noreply.github.com>
2024-04-02 14:28:43 +0900

    [Silver IV] Title: 수 찾기, Time: 420 ms, Memory: 46712 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,24 @@
+#1920번 수 찾기
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if arr[mid] == target:
+            return 1
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return 0
+
+#import sys
+#input = sys.stdin.readline
+n = input()
+lst = list(map(int,input().split()))
+lst.sort()
+m = input()
+lst_input = list(map(int,input().split()))
+for j in lst_input :
+    print(binary_search(lst,j))
